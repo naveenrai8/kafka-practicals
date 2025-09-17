a841bb0ef3cca04dda38b311555bad2a128794f3
Naveen Rai <naveenrai8@gmail.com>
2025-09-17 08:17:26 +0530

    refactor producer and consumer config

--- kafka-features/src/main/java/com/nr/kafkafeatures/config/KafkaConfig.java
+++ kafka-features/src/main/java/com/nr/kafkafeatures/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.nr.kafkafeatures.config;
 
+import com.nr.kafkafeatures.model.Student;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -17,9 +18,14 @@ public class KafkaConfig {
 
     @Bean
     public ProducerFactory<String, String> kafkaStringValueProducerFactory() {
-        Map<String, Object> kafkaConfigProperties = new HashMap<>();
-        kafkaConfigProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        kafkaConfigProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        Map<String, Object> kafkaConfigProperties = commonProducerConfig();
+        kafkaConfigProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(kafkaConfigProperties);
+    }
+
+    @Bean
+    public ProducerFactory<String, Student> kafkaStudentValueProducerFactory() {
+        Map<String, Object> kafkaConfigProperties = commonProducerConfig();
         kafkaConfigProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(kafkaConfigProperties);
     }
@@ -27,9 +33,7 @@ public class KafkaConfig {
 
     @Bean
     public ConsumerFactory<String, String> kafkaStringValueConsumerFactory() {
-        Map<String, Object> kafkaConfigProperties = new HashMap<>();
-        kafkaConfigProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        kafkaConfigProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        Map<String, Object> kafkaConfigProperties = commonConsumerConfig();
         kafkaConfigProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(kafkaConfigProperties);
     }
@@ -44,5 +48,19 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(kafkaStringValueConsumerFactory());
         return factory;
+    }
+
+    private Map<String, Object> commonConsumerConfig() {
+        Map<String, Object> kafkaConfigProperties = new HashMap<>();
+        kafkaConfigProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        kafkaConfigProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return kafkaConfigProperties;
+    }
+
+    private Map<String, Object> commonProducerConfig() {
+        Map<String, Object> kafkaConfigProperties = new HashMap<>();
+        kafkaConfigProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        kafkaConfigProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return kafkaConfigProperties;
     }
 }
